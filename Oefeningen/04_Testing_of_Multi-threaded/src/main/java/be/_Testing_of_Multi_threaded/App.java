88e4cede260965f84d59be3ac7cfeda7c9041182
nicolasblancorequejo <nicolas.blancorequejo@gmail.com>
2016-10-13 15:29:45 +0200

    04 Threads enz

---
+++ Oefeningen/04_Testing_of_Multi-threaded/src/main/java/be/_Testing_of_Multi_threaded/App.java
@@ -0,0 +1,13 @@
+package be._Testing_of_Multi_threaded;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
